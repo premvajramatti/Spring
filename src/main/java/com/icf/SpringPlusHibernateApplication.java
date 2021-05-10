046ee6beb715f17ec6a3d8a2f27608fb80e22d32
Vinod <vinod121088@gmail.com>
2021-05-10 20:33:57 +0530

    created project

---
+++ src/main/java/com/icf/SpringPlusHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.icf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlusHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlusHibernateApplication.class, args);
+	}
+
+}
